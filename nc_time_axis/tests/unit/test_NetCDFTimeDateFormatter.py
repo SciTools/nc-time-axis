59c567d307eb8ced44f2c95b393c93ba571e486c
Spencer Clark <spencerkclark@gmail.com>
2021-08-10 18:39:02 -0400

    Harmonize label format and tick resolution choosing logic (#79)

--- nc_time_axis/tests/unit/test_NetCDFTimeDateFormatter.py
+++ nc_time_axis/tests/unit/test_NetCDFTimeDateFormatter.py
@@ -7,20 +7,20 @@ from nc_time_axis import NetCDFTimeDateFormatter
 
 
 class Test_pick_format(unittest.TestCase):
-    def check(self, ndays):
+    def check(self, resolution):
         locator = mock.MagicMock()
         formatter = NetCDFTimeDateFormatter(
             locator, "360_day", "days since 2000-01-01 00:00"
         )
-        return formatter.pick_format(ndays)
+        return formatter.pick_format(resolution)
 
     def test(self):
-        self.assertEqual(self.check(0.1), "%H:%M:%S")
-        self.assertEqual(self.check(0.6), "%H:%M")
-        self.assertEqual(self.check(5), "%Y-%m-%d %H:%M")
-        self.assertEqual(self.check(40), "%Y-%m-%d")
-        self.assertEqual(self.check(300), "%Y-%m")
-        self.assertEqual(self.check(1000), "%Y")
+        self.assertEqual(self.check("SECONDLY"), "%H:%M:%S")
+        self.assertEqual(self.check("MINUTELY"), "%H:%M")
+        self.assertEqual(self.check("HOURLY"), "%Y-%m-%d %H:%M")
+        self.assertEqual(self.check("DAILY"), "%Y-%m-%d")
+        self.assertEqual(self.check("MONTHLY"), "%Y-%m")
+        self.assertEqual(self.check("YEARLY"), "%Y")
 
 
 if __name__ == "__main__":

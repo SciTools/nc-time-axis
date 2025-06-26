7fcc5873500ab7bb0494fb65e7801cb08b92b5fd
Bill Little <bill.james.little@gmail.com>
2025-06-26 17:01:02 +0100

    fix ruff=0.12 non-compliances

--- src/nc_time_axis/tests/unit/test_CalendarDateTime.py
+++ src/nc_time_axis/tests/unit/test_CalendarDateTime.py
@@ -29,6 +29,17 @@ class Test___eq__(unittest.TestCase):
 
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
+class Test__hash__(unittest.TestCase):
+    def test(self):
+        expected_datetime = cftime.datetime(1967, 7, 22, 3, 6)
+        expected_calendar = "360_day"
+        cdt = CalendarDateTime(expected_datetime, expected_calendar)
+        expected = hash((expected_datetime, expected_calendar))
+        actual = hash(cdt)
+        self.assertEqual(actual, expected)
+
+
+@pytest.mark.filterwarnings("ignore::DeprecationWarning")
 class Test__ne__(unittest.TestCase):
     def setUp(self):
         self.cdt = CalendarDateTime(cftime.datetime(1967, 7, 22, 3, 6), "360_day")
